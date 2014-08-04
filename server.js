95b22a085a807ebe8684fec0faad2a0affc46465
Per Nilsson <pnilsson@walmartlabs.com>
2014-08-04 02:47:10 -0700

    Add Good logging

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var Hapi = require('hapi');
+var Good = require('good');
 var routes = require('./routes');
 
 var port = process.env.PORT || 8000;
 var host = '0.0.0.0';
-
 var server = new Hapi.Server(host, port);
-
 
 server.route(routes);
 
-server.start(function () {
-  console.log(['Time API server started at http://', host, ':', port].join(''));
+server.pack.register(Good, function (err) {
+  if (err) {
+    throw err;
+  }
+
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
 });
+
 
